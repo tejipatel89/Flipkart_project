dc5859066be2522e82b6a88fdf46e86fad9529bf
TEj <teji_patel89@yahoo.com>
2023-01-03 01:24:09 +0530

    add

--- Project_Flipkart/src/main/java/Base/BaseClass.java
+++ Project_Flipkart/src/main/java/Base/BaseClass.java
@@ -4,14 +4,13 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
 
 public class BaseClass {
 	
 	public static WebDriver driver;
 
-@Test
-	public static void browser  () {
+
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		//System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\Selenium Project\\345dipuo\\chromedriver.exe");
